532de11354ad0405e6f9cadc0e7c8812d4bedd53
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-03-07 18:42:35 -0300

    feat: init project

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import love from "eslint-config-love";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  love,
+  {
+    rules:  {
+      "@typescript-eslint/class-methods-use-this": "off",
+      "eslint-comments/require-description": "off",
+      "@typescript-eslint/strict-boolean-expressions": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/prefer-destructuring": "off"
+    }
+  }
+];
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  transform: {
+    ".+\\.ts$": ["ts-jest",{}],
+  },
+};

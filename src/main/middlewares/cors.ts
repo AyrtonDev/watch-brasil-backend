cd8e0ee7b0b09e3ceec61f52f87355be2f81905f
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-03-10 03:06:15 -0300

    feat: ajust headers

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,8 +1,8 @@
 import type { Request, Response, NextFunction } from "express";
 
 export const cors = (req: Request, res: Response, next: NextFunction): void => {
-  res.set('access-controll-allow-origin', '*')
-  res.set('access-controll-allow-headers', '*')
-  res.set('access-controll-allow-methods', '*')
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Headers', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next()
 }

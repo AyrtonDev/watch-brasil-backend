cd3145617886fe9bcd2f587a27eb06f723714f47
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-03-10 01:32:49 -0300

    feat: set env of production

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,16 +1,20 @@
 import { Router, type Express } from 'express'
 import fs from 'fast-glob'
 
+const isProduction = process.env.NODE_ENV === 'production';
+const baseDir = isProduction ? 'dist' : 'src';
+const fileExtension = isProduction ? 'js' : 'ts';
+
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  const files = fs.async('**/src/main/routes/**-routes.ts')
+  const files = fs.async(`**/${baseDir}/main/routes/**-routes.${fileExtension}`)
 
   files
     .then(resolve => resolve.map(async file => {
       const { default: route } = await import(`../../../${file}`)
       route(router)
     }))
-    // eslint-disable-next-line no-console
+     
     .catch((error: unknown) => { console.error(error); })
 }

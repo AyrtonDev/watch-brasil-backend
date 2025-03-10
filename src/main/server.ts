a43da05ef8078e0a49beaf3673150ad48335f16a
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-03-10 02:28:08 -0300

    feat: add cors

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,17 @@
 import { PostgreeHelper } from '../infra/db/postgreedb/helpers/postgree-helper';
 import app from './config/app'
 import dotenv from 'dotenv';
+import cors from 'cors'
 
 dotenv.config();
 
 const port = process.env.PORT
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 PostgreeHelper.connect()
   .then(() => {
